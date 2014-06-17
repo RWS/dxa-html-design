c08fdc210b77a58e0189562c9d74f51f4b56a475
Pavlo Sadovyi <pavlosadovyi@gmail.com>
2014-06-17 15:42:00 +0300

    Youtube issue, location page fixes

--- src/system/assets/scripts/videos.js
+++ src/system/assets/scripts/videos.js
@@ -2,4 +2,8 @@
 
 	$("body").fitVids();
 
+	$('.embed-video > button').on('click', function() {
+		$(this).parent().html('<iframe width="960" height="720" src="//www.youtube.com/embed/'+$(this).data('video')+'" frameborder="0" allowfullscreen></iframe>');
+	});
+
 })($);
