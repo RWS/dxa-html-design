d7609592c565f1cf3501d54a6aa7efcad8eb2b6b
Bart Koopman <bkoopman@sdl.com>
2014-08-13 19:05:08 +0200

    TSI-281 set HD resolution of youtube videos

--- src/system/assets/scripts/videos.js
+++ src/system/assets/scripts/videos.js
@@ -3,7 +3,7 @@
 	$("body").fitVids();
 
 	$('.embed-video > button').on('click', function() {
-		$(this).parent().html('<iframe width="960" height="720" src="//www.youtube.com/embed/'+$(this).data('video')+'?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>').fitVids();
+		$(this).parent().html('<iframe width="1280" height="720" src="//www.youtube.com/embed/'+$(this).data('video')+'?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>').fitVids();
 	});
 
 })(jQuery);
