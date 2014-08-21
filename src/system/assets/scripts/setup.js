92e2890bd32b3aaf6ca378b7737856091b90d9d9
Will Price <willpricework@gmail.com>
2014-08-21 13:19:02 +0200

    Added check for small width images to address sporadic mis-calculation of image heights (see TSI-472)

--- src/system/assets/scripts/setup.js
+++ src/system/assets/scripts/setup.js
@@ -35,9 +35,12 @@ $(document).ready(function() {
 		if ($(this).is(':visible'))
 		{
 			var aspect = $(this).attr("data-aspect");
-			if (!isNaN(aspect) && aspect>0)
+			var width = $(this).width();
+			//there is a strange intermittent issue where item width is misrepresented (as a small number)
+			//so as a workaround we skip images with small widths (these should load quickly in any case)
+			if (!isNaN(aspect) && aspect>0 && width>120)
 			{
-				$(this).height(Math.round($(this).width()/aspect));
+				$(this).height(Math.round(width/aspect));
 			}
 		}
 	});
