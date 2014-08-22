efd9336d0b49630352ceb8bca2d2f943ab2b68ca
Pavlo Sadovyi <pavlosadovyi@gmail.com>
2014-08-22 14:26:46 +0300

    TSI-436 Implemented synchronizing between regular tab list and tab dropdown list.

--- src/system/assets/scripts/tabs.js
+++ src/system/assets/scripts/tabs.js
@@ -1,14 +1,39 @@
 (function($) {
 
-	$('a[data-toggle="tab"]').on('click', function (e) {
-	  $(e.target).tab('show'); // activated tab
-	  $(e.relatedTarget).tab('hide'); // previous tab
-	  return false;
-	});
+  // Tabs
 
-	$('.tab-select').on('change', function (e) {
-	  $(e.target.selectedOptions[0]).tab('show'); // activated tab
-	  return false;
-	});
+  $('.tab-container').on('tab.change', function(e, tab) {
+    var activeTab = $(this).data('active-tab'),
+        nextTab = tab.target.hash ? tab.target.hash
+                                  : tab.target.selectedOptions[0].dataset.target;
+
+    if (activeTab != nextTab) {
+      // Store active tab in tab container
+      $(this).attr('data-active-tab', nextTab);
+
+      // Changing tab by click on link
+      if (tab.target.hash) {
+        $(tab.target).tab('show'); 
+        $(tab.relatedTarget).tab('hide');
+        $(this).find('select.tab-group option[data-target="' + nextTab + '"]').prop('selected', true);
+      }
+      // Changing tab by select control
+      else {
+        $(this).find('.tab-group a[href="' + nextTab + '"]').tab('show');
+      }
+    }
+
+    return false;
+  });
+
+  // Changing tab by select control
+  $('.tab-group').on('change', function(e) {
+    $(this).closest('.tab-container').trigger('tab.change', e);
+  });
+
+  // Changing tab by click on link
+  $('.tab-group a[data-toggle="tab"]').on('click', function(e) {
+    $(this).closest('.tab-container').trigger('tab.change', e);
+  });
 
 })(jQuery);
