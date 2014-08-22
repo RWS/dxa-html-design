f57fb0ae4cd3fcc1ec1a4f16c997f38369c646b3
Pavlo Sadovyi <pavlosadovyi@gmail.com>
2014-08-22 16:26:25 +0300

    Tabs IE fixed

--- src/system/assets/scripts/setup.js
+++ src/system/assets/scripts/setup.js
@@ -54,25 +54,4 @@ $(document).ready(function() {
 			}
 		}
 	});
-
-	// Javascript to enable link to tab
-	var hash = document.location.hash;
-	var prefix = "tab_";
-	if (hash) {
-		if ($('.dropdown').is(':visible')) {
-		  $('.dropdown select option[data-target='+hash.replace(prefix,"")+']').prop('selected', true).tab('show');
-		}
-		else {
-		  $('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');  
-		}
-	} 
-
-	// Change hash for page-reload
-	$('.nav-tabs a').on('shown.bs.tab', function (e) {
-		window.location.hash = e.target.hash.replace("#", "#" + prefix);
-	});       
-
-	$('.dropdown select').on('change', function (e) {
-		window.location.hash = e.target[e.target.selectedIndex].dataset.target.replace("#", "#" + prefix);
-	});
 });
--- src/system/assets/scripts/tabs.js
+++ src/system/assets/scripts/tabs.js
@@ -1,21 +1,31 @@
 (function($) {
 
-  // Tabs
+  // Change active tab after page is loaded if we have hash-link to tab in URL
+  var hash = document.location.hash,
+      prefix = "tab_";
 
+  if (hash) {
+    var newHash = hash.replace(prefix,'');
+    $('select.tab-group').is(':visible') ?
+      $('.tab-group option[value=' + newHash + ']').prop('selected', true).tab('show').closest('.tab-container').attr('data-active-tab', newHash) :
+      $('.tab-group a[href=' + newHash + ']').tab('show').closest('.tab-container').attr('data-active-tab', newHash);  
+  } 
+
+  // Tabs
   $('.tab-container').on('tab.change', function(e, tab) {
     var activeTab = $(this).data('active-tab'),
         nextTab = tab.target.hash ? tab.target.hash
-                                  : tab.target.selectedOptions[0].dataset.target;
+                                  : tab.target[tab.target.selectedIndex].value;
 
     if (activeTab != nextTab) {
       // Store active tab in tab container
-      $(this).attr('data-active-tab', nextTab);
+      $(this).data('active-tab', nextTab);
 
       // Changing tab by click on link
       if (tab.target.hash) {
         $(tab.target).tab('show'); 
         $(tab.relatedTarget).tab('hide');
-        $(this).find('select.tab-group option[data-target="' + nextTab + '"]').prop('selected', true);
+        $(this).find('select.tab-group option[value="' + nextTab + '"]').prop('selected', true);
       }
       // Changing tab by select control
       else {
@@ -29,11 +39,14 @@
   // Changing tab by select control
   $('.tab-group').on('change', function(e) {
     $(this).closest('.tab-container').trigger('tab.change', e);
+    document.location.hash = e.target[e.target.selectedIndex].value.replace('#', '#' + prefix);
   });
 
   // Changing tab by click on link
-  $('.tab-group a[data-toggle="tab"]').on('click', function(e) {
+  $('.tab-group a').on('click', function(e) {
     $(this).closest('.tab-container').trigger('tab.change', e);
+    document.location.hash = e.target.hash.replace('#', '#' + prefix);
+    return false;
   });
 
 })(jQuery);
