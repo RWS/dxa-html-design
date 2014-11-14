0987a34fae1bbd2111a2ad96fb0398abff135ea3
Bart Koopman <bkoopman@sdl.com>
2014-11-14 11:23:33 +0100

    TSI-671 created function and added timeout to document ready call

--- src/system/assets/scripts/xpm.js
+++ src/system/assets/scripts/xpm.js
@@ -1,11 +1,21 @@
 /**
 * XPM specific functions
 */
+
+(function( $ ) {
+    $.fn.checkIfInXpm = function() {
+		if (typeof Tridion != "undefined" && typeof Tridion.Web.UI.SiteEdit != "undefined") {
+			window.console.log("in XPM");
+		} else {
+			window.console.log("not in XPM");
+		}
+    }; 
+}( jQuery ));
+
 $(document).ready(function() {
-	if (typeof Tridion != "undefined" && typeof Tridion.Web.UI.SiteEdit != "undefined") {
-		window.console.log("in XPM");
-		
-	} else {
-		window.console.log("not in XPM");
-	}
+	// wait 5 seconds after document ready and check if we are in XPM
+	setTimeout(function() {
+		$("html").checkIfInXpm();
+	}, 5000);
 });
+
